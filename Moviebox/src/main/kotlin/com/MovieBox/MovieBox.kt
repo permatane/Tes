e122b16c6840ad939c88b7265ccecf5e993a9113
Matane <permatamailbox@gmail.com>
2026-07-20 17:05:32 +0700

    Update MovieBox.kt

--- Moviebox/src/main/kotlin/com/MovieBox/MovieBox.kt
+++ Moviebox/src/main/kotlin/com/MovieBox/MovieBox.kt
@@ -47,7 +47,8 @@ import javax.crypto.spec.SecretKeySpec
 import kotlin.math.max
 
 class MovieBox : MainAPI() {
-    override var mainUrl = "https://api.inmoviebox.com"
+ //   override var mainUrl = "https://api.inmoviebox.com"
+    override var mainUrl = "https://api4sg.aoneroom.com"
     override var name = "MovieBox"
     override val hasMainPage = true
     override var lang = "id"
