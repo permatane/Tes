3a239ceda61b2f480689679c55524f4528673d3a
Matane <permatamailbox@gmail.com>
2026-07-20 14:52:20 +0700

    Update build.gradle.kts

--- Moviebox/build.gradle.kts
+++ Moviebox/build.gradle.kts
@@ -29,6 +29,6 @@ cloudstream {
 
     iconUrl = "https://www.google.com/s2/favicons?domain=https://moviebox.ph&sz=%size%"
 
-    requiresResources = true
+   // requiresResources = true
     isCrossPlatform = false
 }
